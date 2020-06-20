a7554e9df09a79395ee9042e97009c5ddb40c655
henroFall <henryfell@gmail.com>
2020-06-20 12:58:40 -0500

    Create updater.sh
    Changed to ensure NOT root, to run docker-compose updates only if ~/docker exists (hence, no root).
    
    Need to merge NVRChecker into here with condition like for docker, and any other linux server update scripts.
    
    Also should instlal git onto a NAS here, expose a share, and have that NAS git sync this and other scripts? Then have updater sync to them rather than run directly from there?

---
+++ updater.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+rootCheck() {
+    if [ $(id -u) = 0 ]; 
+    then
+        echo -e "\e[41m I am root! Run this WITHOUT SUDO, this script has SUDO where needed. \e[0m"
+        exit 1
+    fi
+}
+
+rebootCheck() {
+
+    if [ -f /var/run/reboot-required ]; then
+      cat /var/run/reboot-required
+    else echo -e '\e[93mNo reboot is required.\e[0m'
+    fi
+}
+
+check_exit_status() {
+
+    if [ $? -eq 0 ]
+    then
+        echo
+       #echo -e "\e[93mSuccess\e[0m"
+       #echo
+    else
+      echo
+      echo -e "\e[93mERROR Process Failed!\e[0m"
+      echo
+      if [[ $1 == '-a' ]]
+      then
+        echo -e "\e[41m AUTO MODE IS ENABLED, EXITING.. \e[0m"
+        echo
+        exit 1
+      else
+        read -p "The last command exited with an error. Exit script? (yes/no)? " answer
+        if [ "$answer" == "yes" ]
+        then
+          exit 1
+        fi
+      fi
+    fi
+}
+
+greeting() {
+
+    echo -e "\e[93m--------------------------------------------------------------"
+    echo -e "\e[93mHello, $USER. Updating all containers I know about and the OS."
+    echo -e "\e[93m--------------------------------------------------------------\e[0m"
+    echo
+}
+
+updateOS() {
+
+    echo -e "\e[93mQuietly updating package information...\e[0m "
+    sudo apt update -qq;
+    check_exit_status $1
+
+    sudo apt -y upgrade;
+    check_exit_status $1
+}
+
+updateDocker() {
+    echo -e "\e[93mChecking/Pulling Fresh Docker Containers...-\e[0m"
+    dockerHome=~/docker
+    composeFile=docker-compose.yml
+	if [ -d "$dockerHome" ]; then
+      cd $dockerHome
+      check_exit_status $1
+      shopt -s nullglob
+      for dname in *; do
+          cd $dname
+        if test -f "$composeFile"; then
+          echo -e "\e[93mOperating on $dname -\e[0m"
+          check_exit_status $1
+          sudo docker-compose down
+          check_exit_status $1
+          sudo docker-compose pull
+          check_exit_status $1
+          sudo docker-compose up -d
+          check_exit_status $1
+        fi
+          cd /home/nuc/docker
+          check_exit_status $1
+      done
+	else
+	  echo -e "\e[93mThere are no docker files at $dockerHome -\e[0m"
+	fi
+}
+
+pruneDocker() {
+    echo -e "\e[93mCleaning up Docker fragments...\e[0m"
+    sudo docker system prune -f
+    check_exit_status $1
+}
+
+showDocker() {
+    echo -e "\e[93m--------------------------------------------------------------"
+    echo -e "\e[93mHello again, $USER. Here are the running containers.."
+    echo -e "\e[93m--------------------------------------------------------------\e[0m"
+    echo
+    docker ps
+    echo -e "\e[93m--------------------------------------------------------------\e[0m"
+}
+
+leave() {
+    echo -e "\e[93mUpdate Complete\e[0m"
+    exit
+}
+
+greeting $@
+rootCheck $@
+updateOS $@
+updateDocker $@
+pruneDocker $@
+showDocker $@
+leave $@
+rebootCheck $@
