bbbf2b7e9f862e652968acc828631e40e3853aae
henroFall <henryfell@gmail.com>
2021-08-10 17:37:29 -0500

    removed Transmission & Jackett enable/disable

--- norddown.sh
+++ norddown.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ -x /usr/bin/nordvpn ]; then
-    if [ -d /home/nuc/docker/transmission/ ]; then
-        cd /home/nuc/docker/transmission
-        docker-compose down
-        cd /home/nuc/docker/jackett
-        docker-compose down
-    fi
+#    if [ -d /home/nuc/docker/transmission/ ]; then
+#        cd /home/nuc/docker/transmission
+#        docker-compose down
+#        cd /home/nuc/docker/jackett
+#        docker-compose down
+#    fi
 nordvpn set killswitch disable
 nordvpn d
 fi
--- nordup.sh
+++ nordup.sh
@@ -2,10 +2,10 @@
 if [ -x /usr/bin/nordvpn ]; then
     nordvpn c
     nordvpn set killswitch enable
-    if [ -d /home/nuc/docker/transmission/ ]; then
-        cd /home/nuc/docker/transmission
-        docker-compose up --no-recreate --detach
-        cd /home/nuc/docker/jackett
-        docker-compose up --no-recreate --detach
-    fi
+#    if [ -d /home/nuc/docker/transmission/ ]; then
+#        cd /home/nuc/docker/transmission
+#        docker-compose up --no-recreate --detach
+#        cd /home/nuc/docker/jackett
+#        docker-compose up --no-recreate --detach
+#    fi
 fi
